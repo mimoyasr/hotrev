fcc969390cf2de444b17d7586ee2e561ed8b426b
omniaMagd93 <omniamagd1993@gmail.com>
2018-04-23 00:43:36 +0200

    connect Receiptionist controller with its view

--- app/Http/Controllers/ReceptionistController.php
+++ app/Http/Controllers/ReceptionistController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+
+use App\Receptionist;
+
 class ReceptionistController extends Controller
 {
     //create receiptionist here
+
+    public function index()
+    {
+    	// /$posts = Post::all();
+        return view('Receiptionist.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+//omnia//
+Route::get('receiptionList','ReceptionistController@index')->name('receiptionList.index');
+//--------------------------------------------------------//
