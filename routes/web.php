ec810937e8938b9a1df795d3a5760406d8d8f9df
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-04-23 21:43:04 +0200

    fix web.php conflict

--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-//omnia//
-//receiption routes
-Route::get('receiptionist','ReceptionistController@index')->name('receiptionist.index');
-//-------------------------------------------------------------------------------------//
-<<<<<<< HEAD
-//floorroutes//
-=======
-//floor routes
 
->>>>>>> 734077e82ea12c83c1b174e23c7a93f3ec743cdb
+Route::get('receiptionist','ReceptionistController@index')->name('receiptionist.index');
+
+
+
 Route::get('floors','FloorController@index')->name('floors.index');
 Route::get('floors/create','FloorController@create')->name('floors.create');
 Route::post('floors/store','FloorController@store')->name('floors.store');
-//--------------------------------------------------------//
+
 
