6957e7c4e7d87d929e014cc10db1c8f6525067b6
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-04-27 21:42:32 +0200

    add model Reservation

---
+++ app/Reservation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+use App\Client;
+use App\Room;
+
+class Reservation extends Model
+{
+    protected $fillable = [
+        'client_id',
+        'room_id',
+        'accompany',
+        'paid_price'
+    ];
+
+    function client()
+    {
+        return $this->hasOne(Client::class);
+    }
+
+    function room()
+    {
+        return $this->hasOne(Room::class);
+    }
+
+}
