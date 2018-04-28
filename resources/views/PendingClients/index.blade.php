4ce074260bb6d2e1087d8b4b6ac725cdaadadd82
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-04-28 20:35:58 +0200

    reformat pending clients view

--- resources/views/PendingClients/index.blade.php
+++ resources/views/PendingClients/index.blade.php
@@ -1,31 +1,21 @@
 @extends("layouts.base")
-
-
 @push('scripts')
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
-
-
     <script type="text/javascript" charset="utf-8">
-
-
         function Approve(id) {
             var target_url = '/pendingclients/' + id;
             $.ajax({
                 type: 'PUT',
-                url: target_url, //Make sure your URL is correct
-                dataType: 'json', //Make sure your returning data type dffine as json
+                url: target_url,
+                dataType: 'json',
                 data: {"_token": "{{ csrf_token() }}"},
                 success: function (data) {
-
-
                     $('#users-table').DataTable().draw(false)
-                    // alert(data.success);
                 },
                 error: function () {
                     alert("Unexpected ERROR");
-
                 },
             });
         }
@@ -50,22 +40,19 @@
             });
         });
     </script>
-    @endpush
-
-
+@endpush
 
 @section('content')
-<table class="table table-striped table-bordered dt-responsive nowrap" style="width:100%" id="users-table">
-    <thead>
-    <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Created At</th>
-        <th>Updated At</th>
-        <th>actions</th>
-
-    </tr>
-    </thead>
-</table>
+    <table class="table table-striped table-bordered dt-responsive nowrap" style="width:100%" id="users-table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+            <th>actions</th>
+        </tr>
+        </thead>
+    </table>
 @stop
