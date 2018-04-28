bc6d77223f0950a5fa4551be468f36d29ffe921b
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-04-28 20:32:59 +0200

    add pending clients to admin panel

--- resources/views/PendingClients/index.blade.php
+++ resources/views/PendingClients/index.blade.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pinding Clients</title>
+@extends("layouts.base")
 
-    <style>
-        body {
-            padding-top: 40px;
-        }
-    </style>
 
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css"/>
-    <link rel="stylesheet" type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css
-"/>
-    <link rel="stylesheet" type="text/css"
-          href="https://cdn.datatables.net/responsive/2.2.1/css/responsive.bootstrap4.min.css"/>
-
+@push('scripts')
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
@@ -48,10 +30,31 @@
             });
         }
     </script>
+    <script>
+        $(function () {
+            $('#users-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: '{{Route('pendingclientsdatatablesapprove.index')}}',
+                columns: [
+                    {data: 'id', name: 'clients.id', searchable: true},
+                    {data: 'name', name: 'users.name', searchable: true},
+                    {data: 'email', name: 'email', searchable: false},
+                    {data: 'created_at', name: 'created_at', searchable: false},
+                    {data: 'updated_at', name: 'updated_at', searchable: false},
+                    {data: 'action', name: 'action', orderable: false, searchable: false}
+
+                ],
 
 
-</head>
-<body>
+            });
+        });
+    </script>
+    @endpush
+
+
+
+@section('content')
 <table class="table table-striped table-bordered dt-responsive nowrap" style="width:100%" id="users-table">
     <thead>
     <tr>
@@ -65,29 +68,4 @@
     </tr>
     </thead>
 </table>
-
-
-<script>
-    $(function () {
-        $('#users-table').DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: '{{Route('pendingclientsdatatablesapprove.index')}}',
-            columns: [
-                {data: 'id', name: 'clients.id', searchable: true},
-                {data: 'name', name: 'users.name', searchable: true},
-                {data: 'email', name: 'email', searchable: false},
-                {data: 'created_at', name: 'created_at', searchable: false},
-                {data: 'updated_at', name: 'updated_at', searchable: false},
-                {data: 'action', name: 'action', orderable: false, searchable: false}
-
-            ],
-
-
-        });
-    });
-</script>
-
-
-</body>
-</html>
+@stop
