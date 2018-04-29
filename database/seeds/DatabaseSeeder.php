ab8f7e406b17e95035bbdc4efb9303db6317ab8e
mimoyasr <amryasserhamza@gmail.com>
2018-04-29 14:50:56 +0200

    merge master

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,19 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Role::create(['name' => 'Admin']);
-        // Role::create(['name' => 'Manager']);
-        // Role::create(['name' => 'Receptionist']);
-        // Role::create(['name' => 'Client']);
-        // User::create([
-        //     'name' => 'admin',
-        //     'email'=> 'admin@admin.com',
-        //     'password' => bcrypt(123456),
-        // ])
-        // ->assignRole('Admin')
-        // ->assignRole('Manager')
-        // ->assignRole('Receptionist')
-        // ->assignRole('Client');
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'Manager']);
+        Role::create(['name' => 'Receptionist']);
+        Role::create(['name' => 'Client']);
+        User::create([
+            'name' => 'admin',
+            'email'=> 'admin@admin.com',
+            'password' => bcrypt(123456),
+        ])
+        ->assignRole('Admin')
+        ->assignRole('Manager')
+        ->assignRole('Receptionist')
+        ->assignRole('Client');
         User::create([
             'name' => 'Manger',
             'email'=> 'man@man.com',
@@ -34,7 +34,7 @@ class DatabaseSeeder extends Seeder
         ->assignRole('Manager')
         ->assignRole('Receptionist')
         ->assignRole('Client');
-       // $this->call('CountriesSeeder');
+       $this->call('CountriesSeeder');
     }
     
 }
